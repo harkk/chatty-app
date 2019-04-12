b24e578659354ac3aea808703944080239d3b6b5
Harkamal Khabra <khabra07@gmail.com>
2019-04-12 19:34:35 +0000

    Handling user actions

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,27 @@ class App extends Component {
     super(props)
     this.state = {
       currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     }
+    this.addMessage = this.addMessage.bind(this);
   }
+
+  addMessage(message, username) {
+    const messageObject = {
+      username: this.state.currentUser.name,
+      content: message,
+      id: Math.random()
+    }
+    const messages = this.state.messages.concat(messageObject)
+    this.setState({messages: messages})
+  }
+
   componentDidMount() {
     console.log("componentDidMount <App />");
     setTimeout(() => {
       console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: "Diego", content: "How's it going?" };
+      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
       const messages = this.state.messages.concat(newMessage)
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
@@ -37,11 +38,12 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <Navbar />
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar addMessage={this.addMessage} currentUser={this.state.currentUser}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 export default class ChatBar extends React.Component{
+  onMessageSubmit = (e) => {
+    if (e.keyCode === 13) {
+      this.props.addMessage(e.target.value)
+      e.target.value = ""
+    }
+  }
+
   render(){
     return(
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyDown={this.onMessageSubmit} />
       </footer>
     );
   };
