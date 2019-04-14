ec872481e03d3d168f040a680e0f3bb0db7fe4e8
Harkamal Khabra <khabra07@gmail.com>
2019-04-14 04:50:26 +0000

    send message to server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid = require('uuid/v4');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -20,6 +21,16 @@ const wss = new SocketServer({ server });
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+  ws.on("message", message => {
+    const messageObject = JSON.parse(message);
+    const messageCast = {
+      id: uuid(),
+      content: messageObject.content,
+      username: messageObject.username
+    }
+    console.log(messageCast)
+  })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
--- src/App.jsx
+++ src/App.jsx
@@ -22,24 +22,16 @@ class App extends Component {
     }
     const messages = this.state.messages.concat(messageObject)
     this.setState({messages: messages})
+    this.socket.send(JSON.stringify(messageObject));
   }
 
   componentDidMount() {
     // Connect to WebSockets server
-    const webSocket = new WebSocket('ws://localhost:3001');
-    webSocket.onopen = function (event) {
+    this.socket = new WebSocket('ws://localhost:3001');
+    this.socket.onopen = function (event) {
       console.log("Connected to webSocket server.")
     };
 
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages })
-    }, 3000);
   }
 
   render() {
